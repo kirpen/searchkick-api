e5a7e69e81a3470b00f4ffb8c860ca557cc29d4b
Stas Kravtsov <my-blizz4rd@hotmail.com>
2019-10-30 08:51:24 +0200

    implemented I18n support for ProcessBatchJob

--- lib/searchkick/process_batch_job.rb
+++ lib/searchkick/process_batch_job.rb
@@ -2,7 +2,7 @@ module Searchkick
   class ProcessBatchJob < ActiveJob::Base
     queue_as { Searchkick.queue_name }
 
-    def perform(class_name:, record_ids:, index_name: nil)
+    def perform(class_name:, record_ids:, index_name: nil, locale: I18n.locale)
       # separate routing from id
       routing = Hash[record_ids.map { |r| r.split(/(?<!\|)\|(?!\|)/, 2).map { |v| v.gsub("||", "|") } }]
       record_ids = routing.keys
@@ -27,9 +27,18 @@ module Searchkick
 
       # bulk reindex
       index = klass.searchkick_index(name: index_name)
-      Searchkick.callbacks(:bulk) do
-        index.bulk_index(records) if records.any?
-        index.bulk_delete(delete_records) if delete_records.any?
+      indexer(index, locale: locale, refresh: records, delete: delete_records)
+    end
+
+    private
+
+    # Inherit from Searchkick::ProcessBatchJob and implement whatever you need
+    def indexer(index, locale: I18n.locale, refresh:, delete:)
+      I18n.with_locale(locale) do
+        Searchkick.callbacks(:bulk) do
+          index.bulk_index(records) if records.any?
+          index.bulk_delete(delete_records) if delete_records.any?
+        end
       end
     end
   end
